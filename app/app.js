34e7cdb510091ce707400b5f10969926f8b7e7be
mmarckwo <78805766+mmarckwo@users.noreply.github.com>
2022-09-21 02:17:10 -0400

    Initial commit

---
+++ app/app.js
@@ -0,0 +1,29 @@
+import * as MODEL from "./model.js";
+
+function route() {
+  let hashTag = window.location.hash;
+  let pageID = hashTag.replace("#", "");
+  let pageIDArray = pageID.split("/");
+  pageID = pageIDArray[0];
+  let subPageID = pageIDArray[1];
+
+  if (pageID == "") {
+    MODEL.changePage("home");
+  } else {
+    if (pageID == subPageID) {
+      MODEL.changePage(pageID);
+    } else {
+      MODEL.changePage(pageID, subPageID);
+    }
+  }
+}
+
+function initApp() {
+  $(window).on("hashchange", route);
+  route();
+}
+
+$(document).ready(function () {
+  initApp();
+  initListeners();
+});
---
+++ app/model.js
@@ -0,0 +1,19 @@
+export function changePage(pageID, subPage) {
+  if (subPage == undefined) {
+    $.get(`pages/${pageID}/${pageID}.html`, function (data) {
+      $("#app").html(data);
+    }).fail((error) => {
+      if (error.status == "404") {
+        //   alert("Page not found. Check your URL.");
+      }
+    });
+  } else {
+    $.get(`pages/${pageID}/${subPage}.html`, function (data) {
+      $("#app").html(data);
+    }).fail((error) => {
+      if (error.status == "404") {
+        //   alert("Page not found. Check your URL.");
+      }
+    });
+  }
+}
